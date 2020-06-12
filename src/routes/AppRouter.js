ba8c0f049fae3b0cf10951f2ff9a55e061a1f7cf
dramkov <61782134+dramkov@users.noreply.github.com>
2020-06-12 13:12:38 +0700

    Store user data in user area

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -2,13 +2,12 @@ import React from "react";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 import AddExpensePage from "./../components/AddExpensePage";
-import HelpPage from "./../components/HelpPage";
 import ExpenseDashboardPage from "./../components/ExpenseDashboardPage";
 import EditExpensePage from "./../components/EditExpensePage";
 import NotFoundPage from "./../components/NotFoundPage";
-
 import LoginPage from "../components/LoginPage";
 import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 export const history = createBrowserHistory();
 
@@ -16,10 +15,10 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" component={LoginPage} exact={true} />
+        <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
+
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <Route component={NotFoundPage} />
       </Switch>
