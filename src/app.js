ba7ff65e3bd82f5606db6305ff2eee63d9a95cda
dramkov <61782134+dramkov@users.noreply.github.com>
2020-06-13 20:08:03 +0700

    Adding visual

--- src/app.js
+++ src/app.js
@@ -5,22 +5,14 @@ import AppRouter, { history } from "./routes/AppRouter";
 import configureStore from "./store/configureStore";
 import { startSetExpenses } from "./actions/expenses";
 import { login, logout } from "./actions/auth";
-import getVisibleExpenses from "./selectors/expenses";
+
 import "normalize.css";
 import "react-dates/lib/css/_datepicker.css";
-import "./styles/style.scss";
+import "./styles/styles.scss";
 import { firebase } from "./firebase/firebase";
+import LoadingPage from "./components/LoadingPage.js";
 
 const store = configureStore();
-
-// store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
-// store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
-// store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
-// store.dispatch(setTextFilter(""));
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
@@ -36,7 +28,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading ...</p>, document.getElementById("app"));
+ReactDOM.render(<LoadingPage />, document.getElementById("app"));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
